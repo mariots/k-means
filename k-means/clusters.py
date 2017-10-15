ef38914ce4bdeb7cffeed4b5b1045d9772018457
mariot <mariotjimenez@gmail.com>
2017-10-15 15:12:26 -0500

    Changed project build settings and added franks visualization to project

---
+++ k-means/clusters.py
@@ -0,0 +1,28 @@
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+
+with open('data.txt', 'r') as infile:
+	dim = int(infile.readline().strip())
+	ndata = int(infile.readline().strip())
+	data = []
+	for i in range(dim * ndata):
+		data.append(float(infile.readline().strip()))		
+	data = np.reshape(data, (ndata, dim))
+        
+	clusters = []
+	for i in range(ndata):
+		clusters.append(int(infile.readline().strip()))
+
+cluster_count = len(set(clusters)) // 2
+sns.set_style("whitegrid")
+cmap = ListedColormap(sns.color_palette("nipy_spectral", cluster_count)).colors
+cmap += ListedColormap(sns.color_palette("terrain", cluster_count)).colors
+np.random.shuffle(cmap)
+cmap = ListedColormap(cmap)
+
+fig, ax = plt.subplots()
+ax.scatter(data[:, 0], data[:, 1], c=clusters, cmap=cmap)
+fig.savefig('clusters.png')
+plt.show()
